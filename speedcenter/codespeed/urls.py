4e7525938be0ad144c2180b877c2a5a93bed9729
Camillo Bruni <camillobruni@gmail.com>
2010-06-12 18:53:42 +0200

    adding sources view

--- speedcenter/codespeed/urls.py
+++ speedcenter/codespeed/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.simple import direct_to_template, redirect_to
 
 urlpatterns = patterns('',
     (r'^$', direct_to_template, {'template': 'home.html'}),
+    (r'^sources/$', direct_to_template, {'template': 'codespeed/sources.html'}),
     (r'^about/$', direct_to_template, {'template': 'about.html'}),
 )
 
